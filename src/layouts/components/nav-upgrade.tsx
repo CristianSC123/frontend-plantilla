cc0b10dd51ff5b7c6e35d5a68f2ec2575415ef17
jorge.mendoza <jorge.mendoza@agetic.gob.bo>
2025-08-01 15:38:08 -0400

    Proyecto inicial de Nextjs ts y mui

---
+++ src/layouts/components/nav-upgrade.tsx
@@ -0,0 +1,26 @@
+import type { StackProps } from '@mui/material/Stack';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export function NavUpgrade({ sx, ...other }: StackProps) {
+  return (
+    <Box
+      sx={[
+        {
+          mb: 4,
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+      {...other}
+    >
+
+          <Typography>Laboratorio Superior de Informática</Typography>
+
+    </Box>
+  );
+}
